457185fa6a11f43d789a473133c852688687a528
furkandereli <furkandereli98@gmail.com>
2024-06-12 14:49:06 +0300

    Token

--- IdentityServer/MultiShop.IdentityServer/Controllers/LoginsController.cs
+++ IdentityServer/MultiShop.IdentityServer/Controllers/LoginsController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using MultiShop.IdentityServer.Dtos;
 using MultiShop.IdentityServer.Models;
+using MultiShop.IdentityServer.Tools;
 using System.Threading.Tasks;
 
 namespace MultiShop.IdentityServer.Controllers
@@ -12,18 +13,28 @@ namespace MultiShop.IdentityServer.Controllers
 	public class LoginsController : ControllerBase
 	{
 		private readonly SignInManager<ApplicationUser> _signInManager;
+		private readonly UserManager<ApplicationUser> _userManager;
 
-		public LoginsController(SignInManager<ApplicationUser> signInManager)
+		public LoginsController(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
 		{
 			_signInManager = signInManager;
+			_userManager = userManager;
 		}
 
 		[HttpPost]
 		public async Task<IActionResult> UserLogin(UserLoginDto userLoginDto)
 		{
 			var result = await _signInManager.PasswordSignInAsync(userLoginDto.Username, userLoginDto.Password, false, false);
+			var user = await _userManager.FindByNameAsync(userLoginDto.Username);
+			
 			if (result.Succeeded)
-				return Ok("Giriş başarılı !");
+			{
+				GetCheckAppUserViewModel model = new();
+				model.Username = userLoginDto.Username;
+				model.Id = user.Id;
+				var token = JwtTokenGenerator.GenerateToken(model);
+				return Ok(token);
+			}
 			else
 				return Ok("Kullanıcı Adı ya da şifre hatalı !");
 		}
