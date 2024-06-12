625569e65e42d0f88bf250a3fd8556a33d43f2a6
furkandereli <furkandereli98@gmail.com>
2024-06-12 10:53:26 +0300

    Login

---
+++ IdentityServer/MultiShop.IdentityServer/Controllers/LoginsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.IdentityServer.Dtos;
+using MultiShop.IdentityServer.Models;
+using System.Threading.Tasks;
+
+namespace MultiShop.IdentityServer.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class LoginsController : ControllerBase
+	{
+		private readonly SignInManager<ApplicationUser> _signInManager;
+
+		public LoginsController(SignInManager<ApplicationUser> signInManager)
+		{
+			_signInManager = signInManager;
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> UserLogin(UserLoginDto userLoginDto)
+		{
+			var result = await _signInManager.PasswordSignInAsync(userLoginDto.Username, userLoginDto.Password, false, false);
+			if (result.Succeeded)
+				return Ok("Giriş başarılı !");
+			else
+				return Ok("Kullanıcı Adı ya da şifre hatalı !");
+		}
+	}
+}
