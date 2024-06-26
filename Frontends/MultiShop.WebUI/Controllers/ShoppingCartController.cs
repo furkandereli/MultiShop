1c04b5aaf1d972edc8d9b445810f68785b3dd132
furkandereli <furkandereli98@gmail.com>
2024-06-26 15:07:00 +0300

    Basket

--- Frontends/MultiShop.WebUI/Controllers/ShoppingCartController.cs
+++ Frontends/MultiShop.WebUI/Controllers/ShoppingCartController.cs
@@ -1,12 +1,48 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.BasketDtos;
+using MultiShop.WebUI.Services.BasketServices;
+using MultiShop.WebUI.Services.CatalogServices.ProductServices;
 
 namespace MultiShop.WebUI.Controllers
 {
     public class ShoppingCartController : Controller
     {
-        public IActionResult Index()
+        private readonly IProductService _productService;
+        private readonly IBasketService _basketService;
+
+        public ShoppingCartController(IProductService productService, IBasketService basketService)
         {
+            _productService = productService;
+            _basketService = basketService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.directory1 = "Ana Sayfa";
+            ViewBag.directory2 = "Ürünler";
+            ViewBag.directory3 = "Sepetim";
             return View();
+        }
+
+        public async Task<IActionResult> AddBasketItem(string productId)
+        {
+            var values = await _productService.GetByIdProductAsync(productId);
+            var items = new BasketItemDto
+            {
+                ProductId = values.ProductId,
+                ProductName = values.ProductName,
+                Price = values.ProductPrice,
+                Quantity = 1,
+            };
+
+            await _basketService.AddBasketItem(items);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> RemoveBasketItem(string productId)
+        {
+            await _basketService.RemoveBasketItem(productId);
+            return RedirectToAction("Index");
         }
     }
 }
